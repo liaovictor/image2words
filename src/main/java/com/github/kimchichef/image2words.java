c435653d65ec26d97bc522077c6ae5f01035172f
Your Name <you@example.com>
2019-04-03 13:34:09 +0800

    ;

---
+++ src/main/java/com/github/kimchichef/image2words.java
@@ -0,0 +1,48 @@
+package com.github.kimchichef;
+
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
+
+
+class Req {
+    private String url;
+
+    public Req(String url) {
+        this.url = url;
+    }
+}
+
+class Res {
+    private String result;
+
+    public String getResult() {
+        return this.result;
+    }
+}
+
+
+public class image2words {
+    public static String image2words(String q) {
+        Req aReq = new Req(q);
+        Gson gson = new Gson();
+        String req = gson.toJson(aReq);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(MediaType.get("application/json;charset=utf-8"), req);
+        Request request = new Request.Builder()
+                .url("https://proto.col.ai/f/image2words")
+                .post(body)
+                .build();
+        String res = "res";
+        try (Response response = client.newCall(request).execute()) {
+            res = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Res resj = gson.fromJson(res, Res.class);
+        return resj.getResult();
+    }
+
+
+}
---
+++ src/test/java/com/github/kimchichef/image2wordsTest.java
@@ -0,0 +1,44 @@
+package com.github.kimchichef;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.gson.JsonSyntaxException;
+import org.junit.Test;
+
+/**
+ * Unit test for simple pinyinIME.
+ */
+public class image2wordsTest {
+    /**
+     * Rigorous Test :-)
+     */
+
+//    @Test
+//    public void testCorrectAnswer() {
+//        image2words classUnderTest = new image2words();
+//        String result = "result";
+//
+//        try {
+//            result = classUnderTest.image2words("ni3ha3o");
+//        } catch (JsonSyntaxException e) {
+//            System.out.println("found exception in test");
+//            e.printStackTrace();
+//        }
+//        assertEquals("nǐhǎo", result);
+//    }
+//
+//    @Test
+//    public void testIncorrectAnswer() {
+//        image2words classUnderTest = new image2words();
+//        String result = "result";
+//
+//        try {
+//            result = classUnderTest.image2words(" nihao ");
+//        } catch (JsonSyntaxException e) {
+//            System.out.println("found exception in test");
+//            e.printStackTrace();
+//        }
+//        assertEquals(" nihao ", result);
+//    }
+
+}
